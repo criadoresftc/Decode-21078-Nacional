bee301ba0dfa008e53f8c9f2d14f4339b355e968
Joao0Marcos <80496388+Joao0Marcos@users.noreply.github.com>
2026-02-26 03:17:18 -0300

    (WIP) em progresso Torreta.java
    Pronto para testes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/br/Torreta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/br/Torreta.java
@@ -2,158 +2,144 @@ package org.firstinspires.ftc.teamcode.br;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
-import com.seattlesolvers.solverslib.command.Subsystem;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
-import com.seattlesolvers.solverslib.trajectory.constraint.DifferentialDriveKinematicsConstraint;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+/*TODO: Testar o subsistema utilizando o robô.
+
+ */
+/**
+ *  Implementação de um subsistema para a torreta giratória do nosso robô.
+ *  <br><br>
+ *  Baseado em: <a href="https://docs.wpilib.org/pt/stable/docs/software/advanced-controls/introduction/tuning-turret.html">...</a>
+ *  @author marcosj
+ */
 @Configurable
-public final class Torreta extends SubsystemBase {
-    private class RelatorioControle {
+public class Torreta extends SubsystemBase {
+    private static class RelatorioControle {
         //Armazena informações importantes de um passo do controlador.
         double posicaoAlvo, posicaoMedida, erroMedido, tensaoEletricaRespondida = 0;
         boolean noAlvo;
 
         double momento = 0;
 
-        private void atualizar(double posicaoAlvo, double posicaoMedida, double tensaoEletricaRespondida) {
-            momento = System.nanoTime() / 1E9;
-
+        private void atualizar(double posicaoAlvo, double posicaoMedida, double tensaoEletricaRespondida, double momento) {
             this.posicaoAlvo = posicaoAlvo;
             this.posicaoMedida = posicaoMedida;
             erroMedido = posicaoAlvo - posicaoMedida;
             noAlvo = Math.abs(erroMedido) < tConstantes.tolerancia;
             this.tensaoEletricaRespondida = tensaoEletricaRespondida;
+
+            this.momento = momento;
         }
     }
 
+    /**
+     *  Switch ligar/desligar.
+     */
     public boolean ativo = true;
-
-    public final double posicaoInicial;
-
-    private double posicao = 0;
+    /**
+     *  Posição alvo em graus.
+     */
+    public double posicao;
 
     public final DcMotorEx encoder;
     private final com.qualcomm.robotcore.hardware.CRServo servoMotor;
 
     private final VoltageSensor sensorEnergia;
 
-    private final RelatorioControle relatorio = new RelatorioControle();
+    private final RelatorioControle ultimoRelatorio = new RelatorioControle();
     public Torreta(double posicaoInicial, HardwareMap hardwareMap) {
         encoder = hardwareMap.get(DcMotorEx.class, "intake");
         servoMotor = hardwareMap.get(CRServo.class, "torreta");
 
-        encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        encoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         sensorEnergia = hardwareMap.voltageSensor.iterator().next();
 
-        this.posicaoInicial = posicaoInicial;
+        posicao = posicaoInicial;
     }
 
     @Override
     public void periodic() {
         //-- CONTROLADOR --
         //Aqui é definido como o modelo físico deve reagir diante à mudança dos atributos.
-        double periodo = System.nanoTime() / 1E9 - relatorio.momento;
 
-        double posicaoAtual = posicaoInicial + encoder.getCurrentPosition() / tConstantes.ticksPorRotacao * 360;
-        double posicaoAlvo = posicao;
+        final double posicaoAtual = encoder.getCurrentPosition() / tConstantes.ticksPorRotacao * 360;
+        final double posicaoAlvo = posicao % 360;
 
-        double erro = (posicaoAtual - posicaoAlvo);
+        final double erro = posicaoAtual - posicaoAlvo;
 
-        double velocidadeErro = 0;
-        if(periodo > 1E6) {
-            velocidadeErro = (relatorio.erroMedido - erro) / periodo;
-        }
+        final double momentoAtual = System.nanoTime() / 1E9;
 
         double tensaoEnviada = 0;
         if(ativo &&
+                Math.abs(erro) > tConstantes.tolerancia &&
                 posicao <= tConstantes.limitacaoMaxima &&
-                posicao >= tConstantes.limitacaoMinima &&
-                Math.abs(erro) > tConstantes.tolerancia
+                posicao >= tConstantes.limitacaoMinima
         ) {
-            tensaoEnviada = erro * tConstantes.ganhoProporcional + velocidadeErro * tConstantes.ganhoDerivado;
+            final double periodo = momentoAtual - ultimoRelatorio.momento;
 
+            tensaoEnviada = erro * tConstantes.ganhoProporcional + (erro - ultimoRelatorio.erroMedido) / periodo * tConstantes.ganhoDerivado +
+                    (posicaoAlvo - ultimoRelatorio.posicaoAlvo) / periodo * tConstantes.kV;
+
+            //Adiciona kS posteriormente, pois só é possível obter a movimentação instantânea da posição alvo.
             tensaoEnviada += Math.signum(tensaoEnviada) * tConstantes.kS;
         }
 
         servoMotor.setPower(tensaoEnviada / sensorEnergia.getVoltage());
 
-        relatorio.atualizar(posicaoAlvo, posicaoAtual, tensaoEnviada);
-    }
-
-    public void moverPara(double angulo) {
-        posicao = angulo % 360;
-
-        //Inverte a posição após um limite fixo de 200 e -200.
-        if(posicao > ((270 + 20 + posicaoInicial) % 360)) {
-            posicao -= 360;
-        }
-        else if(posicao < ((-90 - 20 - posicaoInicial) % 360)) {
-            posicao += 360;
-        }
-
-        //Realiza um pré-controle, enviando rapidamente um "chute" em direção a posição alvo.
-        double posicaoAtual = posicaoInicial + encoder.getCurrentPosition() / tConstantes.ticksPorRotacao * 360;
-        double posicaoAlvo = posicao;
-        double erro = posicaoAlvo - posicaoAtual;
-
-        double tensaoEnviada = 0;
-        if(ativo &&
-                posicao <= tConstantes.limitacaoMaxima &&
-                posicao >= tConstantes.limitacaoMinima &&
-                Math.abs(erro) > tConstantes.tolerancia
-        ) {
-            tensaoEnviada = erro * tConstantes.kV;
-        }
-        servoMotor.setPower(tensaoEnviada / sensorEnergia.getVoltage());
-
-        relatorio.atualizar(posicaoAlvo, posicaoAtual, tensaoEnviada);
+        ultimoRelatorio.atualizar(posicaoAlvo, posicaoAtual, tensaoEnviada, momentoAtual);
     }
 
     /**
-     *  Retorna a posição alvo utilizada pelo controlador a cada passo.
+     *  Retorna a posição alvo utilizada pelo controlador no último passo.
      * @return (graus)
      */
     public double obterPosicaoAlvo() {
-        return relatorio.posicaoAlvo;
+        return ultimoRelatorio.posicaoAlvo;
     }
 
     /**
-     *  Retorna a posição atual medida pelo controlador a cada passo.
+     *  Retorna a posição atual medida pelo controlador no último passo.
      * @return (graus)
      */
     public double obterPosicaoAtual() {
-        return relatorio.posicaoMedida;
+        return ultimoRelatorio.posicaoMedida;
     }
 
     /**
-     *  Retorna o erro de posição medido pelo controlador a cada passo.
+     *  Retorna o erro de posição medido pelo controlador no último passo.
      * @return (graus)
      */
-    public double obterErro() {
-        return relatorio.erroMedido;
+    public double obterErroPosicao() {
+        return ultimoRelatorio.erroMedido;
     }
 
     /**
-     *  Retorna o erro de posição medido pelo controlador a cada passo.
-     * @return (graus)
+     *  Retorna se o controlador chegou na posição alvo no último passo.
+     * @return (booleano)
      */
     public boolean estaNaPosicaoAlvo() {
-        return relatorio.noAlvo;
+        return ultimoRelatorio.noAlvo;
     }
 
     /**
-     *  Retorna a tensão elétrica enviada pelo controlador a cada passo.
+     *  Retorna a tensão elétrica enviada pelo controlador no último passo.
      *  @return (volts)
      */
-    public double obterTensaoEnviada() {
-        return relatorio.tensaoEletricaRespondida;
+    public double obterSaida() {
+        return ultimoRelatorio.tensaoEletricaRespondida;
+    }
+
+    public void adicionarUltimoRelatorio(Telemetry telemetria) {
+        telemetria.addData(getName().toUpperCase() + " : " + "Pos Alvo (Graus)" , this::obterPosicaoAlvo);
+        telemetria.addData(getName().toUpperCase() + " : " + "Pos Atual (Graus)" , this::obterPosicaoAtual);
+        telemetria.addData(getName().toUpperCase() + " : " + "no Alvo? (Booleano)", this::estaNaPosicaoAlvo);
+        telemetria.addData(getName().toUpperCase() + " : " + "Erro (Graus)", this::obterErroPosicao);
+        telemetria.addData(getName().toUpperCase() + " : " + "Saída (Volts)", this::obterSaida);
     }
 }
 
@@ -166,9 +152,9 @@ class tConstantes {
     public static double limitacaoMinima = -90;
 
     //Tolerância do controlador (graus)
-    public static double tolerancia = Math.toRadians(1);
+    public static double tolerancia = 0.5;
 
-    //Constantes do FeedForward (kS = volts, kV = volts / graus).
+    //Constantes do FeedForward (kS = volts, kV = volts / graus/s).
     public static double kV = 1;
     public static double kS = 4;
 
