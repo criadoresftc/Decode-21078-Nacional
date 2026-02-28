feb1ebd4606e18d252b626099eb899cf32a5ca11
Joao0Marcos <80496388+Joao0Marcos@users.noreply.github.com>
2026-02-28 04:33:58 -0300

    (WIP) em progresso Intake.java
    Pronto para ser testado

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/br/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/br/Intake.java
@@ -1,41 +1,62 @@
 package org.firstinspires.ftc.teamcode.br;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.util.Timing;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+//TODO: Testar no robô + Adicionar JDocs
+@Configurable
 public class Intake extends SubsystemBase {
-    public enum Tarefa {
-        ENVIAR_ARTEFATO, COLETAR_ARTEFATO, EJETAR_ARTEFATO, SEGURAR_ARTEFATO, FICAR_PARADO
+    //Expõem as constantes utilizadas globalmente (para Dashboard).
+    public static iConstantes constantes = new iConstantes();
+    
+    public enum Modo {
+        ENVIAR_ARTEFATO, COLETAR_ARTEFATO, EJETAR_ARTEFATO, SEGURAR_ARTEFATO, NAO_FAZER_NADA
     }
 
-    public Tarefa tarefa;
+    /**
+     *  Switch ligar/desligar.
+     */
+    public boolean ativo = true;
+    /**
+     *  Modo do controlador.
+     */
+    public Modo modo;
 
     private final DcMotorEx motorEntrada;
     private final DcMotorEx motorSaida;
-
     private final CRServo servoApoioPrimario;
     private final CRServo servoApoioSecundario;
 
     private final DistanceSensor sensorArtefato;
 
-    public Intake(Tarefa tarefaInicial, HardwareMap hardwareMap) {
+    private final Timing.Stopwatch cronometroTempoSemArtefatoDetectado = new Timing.Stopwatch();
+
+    public Intake(Modo modoInicial, HardwareMap hardwareMap) {
         motorEntrada = hardwareMap.get(DcMotorEx.class, "");
         motorSaida = hardwareMap.get(DcMotorEx.class, "");
         servoApoioPrimario = hardwareMap.get(CRServo.class, "");
         servoApoioSecundario = hardwareMap.get(CRServo.class, "");
 
         sensorArtefato = (DistanceSensor) hardwareMap.get(ColorSensor.class, "");
+
+        modo = modoInicial;
     }
 
     @Override
     public void periodic() {
-        final boolean artefatoDetectado = true;
-
-        switch (tarefa) {
+        //-- CONTROLADOR --
+        //Aqui é definido como o modelo físico deve reagir diante à mudança dos atributos.
+        switch (modo) {
             case ENVIAR_ARTEFATO:
                 motorEntrada.setPower(0.5);
                 motorSaida.setPower(0.9);
@@ -45,18 +66,18 @@ public class Intake extends SubsystemBase {
             break;
             case COLETAR_ARTEFATO:
                 motorEntrada.setPower(0.9);
-                if(artefatoDetectado) {
+                if(detectarArtefato()) {
                     motorSaida.setPower(0);
                 } else {
                     motorSaida.setPower(0.5);
                 }
 
-                servoApoioPrimario.setPower(0);
+                servoApoioPrimario.setPower(0.5);
                 servoApoioSecundario.setPower(0);
             break;
             case EJETAR_ARTEFATO:
-                motorEntrada.setPower(-1);
-                motorSaida.setPower(-1);
+                motorEntrada.setPower(-0.9);
+                motorSaida.setPower(-0.9);
 
                 servoApoioPrimario.setPower(0);
                 servoApoioSecundario.setPower(0);
@@ -68,7 +89,7 @@ public class Intake extends SubsystemBase {
                 servoApoioPrimario.setPower(0);
                 servoApoioSecundario.setPower(0);
             break;
-            case FICAR_PARADO:
+            case NAO_FAZER_NADA:
                 motorEntrada.setPower(0);
                 motorSaida.setPower(0);
 
@@ -76,9 +97,36 @@ public class Intake extends SubsystemBase {
                 servoApoioSecundario.setPower(0);
         }
     }
+
+    public boolean detectarArtefato() {
+        final double distanciaDetectada = sensorArtefato.getDistance(DistanceUnit.CM);
+        if(distanciaDetectada <= iConstantes.cutoffDistanciaDetectada) {
+            cronometroTempoSemArtefatoDetectado.start();
+            
+            return true;
+        }
+        
+        if(cronometroTempoSemArtefatoDetectado.isTimerOn()) {
+            return cronometroTempoSemArtefatoDetectado.elapsedTime() <= iConstantes.duracaoBufferDeteccao;
+        }
+        
+        return false;
+    }
+
+    public String obterResumoModo() {
+        return modo.name();
+    }
+
+    public void adicionarDepuracao(Telemetry telemetria) {
+        telemetria.addData(getName().toUpperCase() + " : " + "Artef detectado? (Booleano)" , this::detectarArtefato);
+        telemetria.addData(getName().toUpperCase() + " : " + "Modo" , this::obterResumoModo);
+    }
 }
 
 class iConstantes {
     //Até onde um artefato pode ser detectado pelo sensor de distância (centímetros)
     public static double cutoffDistanciaDetectada = 5;
+
+    //Quanto tempo um artefato deve permanecer como detectado (milissegundos)
+    public static long duracaoBufferDeteccao = 1000;
 }
